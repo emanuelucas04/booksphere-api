ff5f2d604455a77be5d58f87c1463a3c51ffb235
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-12-29 20:33:00 -0300

    fix route comment

--- src/routes.ts
+++ src/routes.ts
@@ -3,18 +3,6 @@ import { usersRouter } from '@users/http/routes/users.routes'
 
 const routes = Router()
 
-/**
- * @openapi
- * /:
- * get:
- *    description: Rota para retornar um hello world.
- *    responses:
- *      200:
- *        description: Retorna um hello world.
- *
- *
- *
- */
 routes.get('/', (request, response) => {
   response.json({ message: 'Hello World' })
 })
