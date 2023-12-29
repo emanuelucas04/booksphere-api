7b853eb7598299526a69720859305121e6f9dc7b
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-12-28 23:06:53 -0300

    config routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,11 +2,13 @@ import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 const port = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!!`)
