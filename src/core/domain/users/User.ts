987249b96b9bb10b851e9c596a67e6bf483352c4
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2023-12-30 11:52:37 -0300

    update user entity

--- src/core/domain/users/User.ts
+++ src/core/domain/users/User.ts
@@ -1,9 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
+@Entity('users')
 export class User {
+  @PrimaryColumn()
   id?: string
+
+  @Column()
   name: string
+
+  @Column()
   email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  avatar?: string
+
+  @CreateDateColumn()
   createdAt: Date
 
   constructor() {
