74b616a2ae7c2b1a0e484ff39b5f735698e074de
Emanuel Lucas <elsa2@aluno.ifal.edu.br>
2024-01-04 00:18:43 -0300

    refactoring IsAuthenticated middleware

--- src/api/middlewares/IsAuthenticatedUser.ts
+++ src/api/middlewares/IsAuthenticatedUser.ts
@@ -3,6 +3,10 @@ import { NextFunction, Request, Response } from 'express'
 import { Secret, verify } from 'jsonwebtoken'
 import authConfig from '@config/auth'
 
+type JwtPayloadProps = {
+  sub: string
+}
+
 export const isAuthenticated = (request: Request, response: Response, next: NextFunction) => {
   const authHeader = request.headers.authorization
   if (!authHeader) {
@@ -10,7 +14,9 @@ export const isAuthenticated = (request: Request, response: Response, next: Next
   }
   const token = authHeader.replace('Bearer ', '')
   try {
-    verify(token, authConfig.jwt.secret as Secret)
+    const decodedToken = verify(token, authConfig.jwt.secret as Secret)
+    const { sub } = decodedToken as JwtPayloadProps
+    request.user = { id: sub }
     return next()
   } catch {
     throw new ApiError('Invalid authentication token', 401)
